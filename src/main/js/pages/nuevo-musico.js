f277e986b341155af1632d24068514f83e9bf27e
RodrigoEdit <elgueraamadorodrigo@gmail.com>
2023-08-07 12:57:38 -0500

    dsad

--- src/main/js/pages/nuevo-musico.js
+++ src/main/js/pages/nuevo-musico.js
@@ -1,19 +1,35 @@
 const React = require('react');
 const client = require('../client');
 const { Link } = require('react-router-dom');
+const { useState } = require('react');
 
-const PageNuevoMusico = () =>{
-    return(
+const PageNuevoMusico = () => {
+
+    const [nombre, setNombre] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        client({
+            method: 'POST',
+            path: '/api/musicos',
+            entity: { nombre: nombre },
+            headers: { 'Content-Type': 'application/json' }
+        }).done(() => {
+            window.location = '/';
+        });
+    }
+
+    return (
         <>
-            <h1>Nuevo Musico</h1>
-            <form>
-                <label>Nombres:</label>
-                <input type="text" name="nombre"></input>
-                <input type='submit' value="Nuevo Músico"></input>
+            <h1>Nuevo Músico</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Nombre</label>
+                <input type="text" id="nombre" name="nombre" onChange={(e)=>setNombre(e.target.value)} />
+                <input type="submit" value="Nuevo Músico" />
             </form>
             <Link to="/">Volver</Link>
         </>
     )
 }
-// npm i react-router-dom 
+
 module.exports = PageNuevoMusico;
