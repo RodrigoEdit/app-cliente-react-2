e18d6a271e94520b66b32794f9bb75e57b2463e4
RodrigoEdit <elgueraamadorodrigo@gmail.com>
2023-08-07 12:19:58 -0500

    upppp

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const App = require('./pages/home');
+const App = require('./nuevo-musico/home');
 
 ReactDOM.render(
 	<App />,
---
+++ src/main/js/pages/nuevo-musico.js
@@ -0,0 +1,16 @@
+const React = require('react');
+const client = require('../client');
+
+
+const PageNuevoMusico = () =>{
+    return(
+        <>
+            <h1>Nuevo Musico</h1>
+            <form>
+                <label>Nombres:</label>
+                <input type="text" name="nombre"></input>
+                <input type='submit' value="Nuevo Músico"></input>
+            </form>
+        </>
+    )
+}
